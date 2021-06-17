49ecfd92409ee6c501c37f6498daf5c8b906aa16
shyamgangane <shyam.ganagne@gmail.com>
2021-06-17 21:33:52 +0530

    Resuable Method

--- src/main/java/_31_BasicProgramAssingment/ReverseString.java
+++ src/main/java/_31_BasicProgramAssingment/ReverseString.java
@@ -1,34 +1,110 @@
 package _31_BasicProgramAssingment;
 
 public class ReverseString {
-	
+
 	public static void main(String[] args) {
-		
+
 		//Reverse a String
 		// Difference between String and StringBuffer
 		//do we have reverse function in String? ---- Ans is-- NO
-		
+
 		String s = "Hello World";
+		String s1 = "Hello Shyam";
+		String s2 = "Hello Sam";
 		
-		//Using for loop
+		ReverseString objRef = new ReverseString();
+		objRef.revString(s);
+		objRef.revString(s1);
+		objRef.revString(s2);
+		System.out.println("-------------------------");
+		reverseString(s);
+		reverseString(s1);
+		reverseString(s2);
+		ReverseString.reverseString(s1);
+		System.out.println("-------------------------");
+		String test = reverseString1(s2);
+		System.out.println(test + "10");
+		System.out.println(reverseString1(s2) + "20");
 		
-		int len = s.length();
+
+//		//Using for loop
+//
+//		int len = s.length();
+//
+//		String rev = "";
+//
+//		for(int i = len-1; i>=0; i--) {
+//
+//			rev = rev + s.charAt(i);
+//		}
+//
+//		System.out.println(rev);
+//
+//		// using StringBuffer class
+//
+				StringBuffer sf = new StringBuffer(s);
+				System.out.println(sf.reverse());
 		
-		String rev = "";
 		
-		for(int i = len-1; i>=0; i--) {
-			
-			rev = rev + s.charAt(i);
-		}
-		
-		System.out.println(rev);
-		
-		// using StringBuffer class
-		
-		StringBuffer sf = new StringBuffer(s);
-		System.out.println(sf.reverse());
-		
-		
+
+
 	}
+
+
+	public void revString(String testString)
+	{
+		int len = testString.length();
+
+		String rev = "";
+
+		for(int i = len-1; i>=0; i--) {
+
+			rev = rev + testString.charAt(i);
+		}
+
+		System.out.println(rev);
+	}
+	
+	/*
+	 * this method is used to reverse the string
+	 * @author Shyam
+	 */
+	public static void reverseString(String testString)
+	{
+		int len = testString.length();
+
+		String rev = "";
+
+		for(int i = len-1; i>=0; i--) {
+
+			rev = rev + testString.charAt(i);
+		}
+
+		System.out.println(rev);
+	}
+	
+	
+	
+	/**
+	 * This method is used to reverse a string
+	 * @author Sarang
+	 * @param testString
+	 * @return the reversed string 
+	 */
+	public static String reverseString1(String testString)
+	{
+		int len = testString.length();
+
+		String rev = "";
+
+		for(int i = len-1; i>=0; i--) {
+
+			rev = rev + testString.charAt(i);
+		}
+
+		return rev;
+	}
+
+
 
 }
---
+++ src/main/java/_31_BasicProgramAssingment/test.java
@@ -0,0 +1,17 @@
+package _31_BasicProgramAssingment;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		
+		ReverseString test = new ReverseString();
+		test.revString("Test the String");
+		
+		ReverseString.reverseString("sbhdcdchdscj");
+		
+		
+
+	}
+
+}
